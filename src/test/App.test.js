80a427dd3b4600e71141e90f457119680b81fdc6
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-11-18 14:10:38 +0700

    update cicd

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -26,7 +26,7 @@ describe('test first render tombol belum di klik', () => {
   })
   test('tulisan di tombol ketika render pertama kali', () => {
     render(<App />)
-    const txt = screen.getByText(/munculkan/i)
+    const txt = screen.getByText(/munculkanaaaa/i)
     expect(txt).toBeInTheDocument();
   })
 })
